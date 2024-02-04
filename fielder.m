d6d9efd52fcd5560419fdfd6fe98532b7c758410
Andrea24900 <118066154+Andrea24900@users.noreply.github.com>
2024-02-04 19:56:18 +0000

    upload of displacement data

--- fielder.m
+++ fielder.m
@@ -2,7 +2,7 @@ function field=fielder(content,field_number)
 %this function builds the 8 char field with the correct spacing
 
 %all fields except for field 1
-if field_number>1
+if (field_number~=10&&field_number~=1)
 field=pad(content,8,"left");
 else
     field=pad(content,8,"right");
