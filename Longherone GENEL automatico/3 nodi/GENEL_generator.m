8ed5414226dec22f2e4c4862d910331f31edefbe
Ancalb <andreacalabria.18@gmail.com>
2024-02-22 12:45:01 +0100

    GENEL trave coda
    Adattato Genel generator per la trave di coda e generato il GEnel

--- Longherone GENEL automatico/3 nodi/GENEL_generator.m
+++ Longherone GENEL automatico/3 nodi/GENEL_generator.m
@@ -3,45 +3,100 @@ close all; clearvars; clc;
 
 fid = fopen('tutti_i_carichi.f06', 'r');
 
+N = 3;
+spostamenti_verticali = zeros(N,N);
+torsioni = zeros(N,N);
 
-spostamenti_verticali = zeros(3,3);
-torsioni = zeros(3,3);
-
-a = 0; b = 0; c = 0; d = 0; e = 0; f = 0;
+a = zeros(N,1);
 while ~feof(fid)
     currentLine = fgetl(fid);
     if length(currentLine)>36
             if currentLine(1:26) == '         12001      G     '
-                a = a+1;
-                if a<=3
-                    spostamenti_verticali(1,a) = str2double(currentLine(43:54));
+                a(1) = a(1)+1;
+                if a(1)<=N
+                    spostamenti_verticali(1,a(1)) = str2double(currentLine(43:54));
                 end
-                if a>=4
-                    torsioni(1,a-3) = str2double(currentLine(103:114));
+                if a(1)>=N+1
+                    torsioni(1,a(1)-N) = str2double(currentLine(103:114));
                 end
             end
 
             if currentLine(1:26) == '         12002      G     '
-                b = b+1;
-                if b<=3
-                    spostamenti_verticali(2,b) = str2double(currentLine(43:54));
+                a(2) = a(2)+1;
+                if a(2)<=N
+                    spostamenti_verticali(2,a(2)) = str2double(currentLine(43:54));
                 end
-                if b>=4
-                    torsioni(2,b-3) = str2double(currentLine(103:114));
+                if a(2)>=N+1
+                    torsioni(2,a(2)-N) = str2double(currentLine(103:114));
                 end
             end
 
             if currentLine(1:26) == '         12003      G     '
-                c = c+1;
-                if c<=3
-                    spostamenti_verticali(3,c) = str2double(currentLine(43:54));
+                a(3) = a(3)+1;
+                if a(3)<=N
+                    spostamenti_verticali(3,a(3)) = str2double(currentLine(43:54));
                 end
-                if c>=4
-                    torsioni(3,c-3) = str2double(currentLine(103:114));
+                if a(3)>=N+1
+                    torsioni(3,a(3)-N) = str2double(currentLine(103:114));
+                end
+            end
+            if currentLine(1:26) == '         12004      G     '
+                a(4) = a(4)+1;
+                if a(4)<=N
+                    spostamenti_verticali(4,a(4)) = str2double(currentLine(43:54));
+                end
+                if a(4)>=N+1
+                    torsioni(4,a(4)-N) = str2double(currentLine(103:114));
+                end
+            end
+            if currentLine(1:26) == '         12005      G     '
+                a(5) = a(5)+1;
+                if a(5)<=N
+                    spostamenti_verticali(5,a(5)) = str2double(currentLine(43:54));
+                end
+                if a(5)>=N+1
+                    torsioni(5,a(5)-N) = str2double(currentLine(103:114));
+                end
+            end
+            if currentLine(1:26) == '         12006      G     '
+                a(6) = a(6)+1;
+                if a(6)<=N
+                    spostamenti_verticali(6,a(6)) = str2double(currentLine(43:54));
+                end
+                if a(6)>=N+1
+                    torsioni(6,a(6)-N) = str2double(currentLine(103:114));
+                end
+            end
+            if currentLine(1:26) == '         12007      G     '
+                a(7) = a(7)+1;
+                if a(7)<=N
+                    spostamenti_verticali(7,a(7)) = str2double(currentLine(43:54));
+                end
+                if a(7)>=N+1
+                    torsioni(7,a(7)-N) = str2double(currentLine(103:114));
+                end
+            end
+            if currentLine(1:26) == '         12008      G     '
+                a(8) = a(8)+1;
+                if a(8)<=N
+                    spostamenti_verticali(8,a(8)) = str2double(currentLine(43:54));
+                end
+                if a(8)>=N+1
+                    torsioni(8,a(8)-N) = str2double(currentLine(103:114));
+                end
+            end
+            if currentLine(1:26) == '         12006      G     '
+                a(9) = a(9)+1;
+                if a(9)<=N
+                    spostamenti_verticali(9,a(9)) = str2double(currentLine(43:54));
+                end
+                if a(9)>=N+1
+                    torsioni(9,a(9)-N) = str2double(currentLine(103:114));
                 end
             end
     end
 end
+
 
 fileID = fopen('GENEL_generated.txt', 'w');
 
@@ -49,30 +104,69 @@ fileID = fopen('GENEL_generated.txt', 'w');
 if fileID == -1
     error('Impossibile aprire il file per la scrittura.');
 end
-stringa1 = 'GENEL        100               2       3       3       3       4       3';
+
+stringa1 = 'GENEL        100               ';
+
 stringa3 = '$              UD               1       1       1       2       1       3';
 stringa4 = '$               1       4       1       5       1       6';
-stringa5 = ['               Z',converti(spostamenti_verticali(1,1)),converti(spostamenti_verticali(2,1)),converti(spostamenti_verticali(3,1)),...
-    converti(spostamenti_verticali(2,2)),converti(spostamenti_verticali(3,2)),converti(spostamenti_verticali(3,3))];
+stringa5 = '               Z';
 
+for j = 1:N
+    if j == 1
+    stringa1 = [stringa1,num2str(j+1),'       3'];
+    else
+    stringa1 = [stringa1,'       ',num2str(j+1),'       3'];
+    end
 
-fprintf(fileID, '%s\n', stringa1);
+    for i = j:N
+        stringa5 = [stringa5,converti(spostamenti_verticali(i,j))];
+    end
+end
+[stringa1,righe] = vai_a_capo_nodi(stringa1);
+for i = 1:righe
+fprintf(fileID, '%s\n', stringa1(i,:));
+end
 fprintf(fileID, '%s\n', stringa3);
 fprintf(fileID, '%s\n', stringa4);
-fprintf(fileID, '%s\n', stringa5);
+
+[stringa5,righe] = vai_a_capo_spostamenti(stringa5);
+for i = 1:righe
+    fprintf(fileID, '%s\n', stringa5(i,:));
+end
+fprintf(fileID, '\n\n');
 
 
-stringa1 = 'GENEL        200               2       5       3       5       4       5';
+stringa1 = 'GENEL        200               ';
+
 stringa3 = '$              UD               1       1       1       2       1       3';
 stringa4 = '$               1       4       1       5       1       6';
-stringa5 = ['               Z',converti(torsioni(1,1)),converti(torsioni(2,1)),converti(torsioni(3,1)),...
-    converti(torsioni(2,2)),converti(torsioni(3,2)),converti(torsioni(3,3))];
+stringa5 = '               Z';
 
 
-fprintf(fileID, '%s\n', stringa1);
+for j = 1:N
+    if j == 1
+    stringa1 = [stringa1,num2str(j+1),'       5'];
+    else
+    stringa1 = [stringa1,'       ',num2str(j+1),'       5'];
+    end
+
+    for i = j:N
+        stringa5 = [stringa5,converti(torsioni(i,j))];
+    end
+end
+[stringa1,righe] = vai_a_capo_nodi(stringa1);
+for i = 1:righe
+fprintf(fileID, '%s\n', stringa1(i,:));
+end
 fprintf(fileID, '%s\n', stringa3);
 fprintf(fileID, '%s\n', stringa4);
-fprintf(fileID, '%s\n', stringa5);
+
+[stringa5,righe] = vai_a_capo_spostamenti(stringa5);
+for i = 1:righe
+    fprintf(fileID, '%s\n', stringa5(i,:));
+end
+
+
 
 fclose(fileID);
 
@@ -87,9 +181,52 @@ end
 
 stringa_numero = num2str(numero);
 for i = 1:6-length(stringa_numero)
-stringa_numero = [stringa_numero,'0'];
+    stringa_numero = [stringa_numero,'0'];
 end
 stringa = [stringa_numero,'-',num2str(posizione)];
 end
 
+function [stringa_out,righe] = vai_a_capo_nodi(stringa_in)
+if strlength(stringa_in) <= 72
+    stringa_out = stringa_in;
+    righe=1;
+else
+    
+    spazi_finali = 136-strlength(stringa_in);
+     vuoto = ' ';
+    for j = 1:spazi_finali-1
+        vuoto = [vuoto,' '];
+    end
+    stringa_out = [stringa_in(1:72);'        ',stringa_in(73:end),vuoto];
+    righe = 2;
+end
+end
 
+function [stringa_out,righe] = vai_a_capo_spostamenti(stringa_in)
+if strlength(stringa_in) <= 72
+    stringa_out = stringa_in;
+    righe=1;
+else
+    esci = 0;
+    stringa_out = stringa_in(1:72);
+    a = 1;
+    caratteri = strlength(stringa_in)-72;
+    while esci == 0
+        if caratteri<=64
+            vuoto = ' ';
+            for j = 1:64-caratteri-1
+                vuoto = [vuoto,' '];
+            end
+        stringa_out = [stringa_out;'        ',stringa_in(73+64*(a-1):end),vuoto];
+        esci = 1;
+        righe = a+1;
+        else
+        stringa_out = [stringa_out;'        ',stringa_in(73+64*(a-1):(72+64*a) )];
+        a = a+1;
+        caratteri = caratteri-64;
+    end
+
+    end
+    
+end
+end
